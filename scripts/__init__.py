84e20ee99dd23d46b356779e79b943fa3b9fb947
Javier-FernandezCarabantes <javignp8@gmail.com>
2025-02-05 22:26:34 +0100

    Delete scripts directory

--- scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-# scripts/__init__.py
-from .Pre_processing import process_H
-from .model_RNN_DAS import load_model
-from .Features import features
-from .run_RNN_das import run_model
-from .grammar import grammar
-from .plots import plot_das_bi, plot_das_grammar
-from .picks import write_pickle, detect_phases, extract_events_to_mseed
