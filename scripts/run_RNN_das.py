b4c9ce13641b2c8a96475776b6a3dece2fb80cfc
Javier Fernández-Carabantes <javignp8@gmail.com>
2025-02-12 14:08:27 +0100

    Add files via upload

--- scripts/run_RNN_das.py
+++ scripts/run_RNN_das.py
@@ -17,23 +17,22 @@ def predict(model, dataloader):
     all_predictions = []
     all_probabilities = []
 
-    softmax = nn.Softmax(dim=2)  # Convert logits to probabilities
+    softmax = nn.Softmax(dim=2)
 
-    with torch.no_grad():  # Disable gradient computation
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+
+    with torch.no_grad():
         for batch in dataloader:
-            # Unpack the batch
-            batch_x = batch[0]  
-            # Move batch to GPU if available
-            batch_x = batch_x.cuda() if torch.cuda.is_available() else batch_x
+            batch_x = batch[0]
+            batch_x = batch_x.to(device)
             
-            # Make predictions
             outputs, _ = model(batch_x)
-            # Process outputs to obtain probabilities and predictions
-            probabilities = torch.softmax(outputs, dim=2)  
+            probabilities = torch.softmax(outputs, dim=2)
             predictions = torch.argmax(probabilities, dim=2)
 
-            all_predictions.append(predictions.cpu().numpy())  # Move predictions to CPU and convert to numpy
-            all_probabilities.append(probabilities.cpu().numpy())  # Move probabilities to CPU and convert to numpy
+            all_predictions.append(predictions.cpu().numpy())
+            all_probabilities.append(probabilities.cpu().numpy())
             
     all_predictions = np.concatenate(all_predictions)
     all_probabilities = np.concatenate(all_probabilities)
@@ -41,33 +40,11 @@ def predict(model, dataloader):
     return all_predictions, all_probabilities
 
 def run_model(model, data):
-    """
-    Function to execute the RNN-DAS model with the provided data.
-    
-    Args:
-        model: The trained RNN-DAS model.
-        data: The input dataloader to be processed.
-    
-    Returns:
-        predictions, probabilities: Model outputs.
-    """
     predictions, probabilities = predict(model, data)  
     return predictions, probabilities
 
 def run_batch_model(model, data, fileids):
-    """
-    Function to execute the RNN-DAS model with the provided data for various samples.
-    
-    Args:
-        model: The trained RNN-DAS model.
-        data: The input data to be processed.
-        fileids: List of events ids
-    
-    Returns:
-        predictions, probabilities: Model outputs.
-    """
     for fileid in fileids:
         predictions, probabilities = predict(model, data)  
         return predictions, probabilities
-
 
